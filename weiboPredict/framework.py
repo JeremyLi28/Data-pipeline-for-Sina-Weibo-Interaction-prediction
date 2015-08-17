fd530d6d8322198366809db0e71c668610432ef0
JeremyLi28 <270768253@qq.com>
2015-08-17 22:51:08 +0800

    fix read csv bug
    When read from csv to pandas data frame, there are some missing lines.
    The problem is because of the single quote.

--- weiboPredict/framework.py
+++ weiboPredict/framework.py
@@ -2,16 +2,16 @@
 # Usage: basic operations
 # Author: Chen Li
 import pandas as pd
-
+import csv
 weibo_train_data = None
 weibo_predict_data = None
 def loadData():
 	global weibo_train_data 
 	weibo_train_data= pd.read_csv('data/weibo_train_data.txt',sep='\t', 
-		names=['uid','mid','time','forward_count','comment_count','like_count','context'])
+		quoting=csv.QUOTE_NONE,names=['uid','mid','time','forward_count','comment_count','like_count','context'])
 	global weibo_predict_data 
 	weibo_predict_data = pd.read_csv('data/weibo_predict_data.txt',sep='\t',
-		names=['uid','mid','time','context'])
+		quoting=csv.QUOTE_NONE,names=['uid','mid','time','context'])
 
 if __name__ == "__main__":
 	loadData()
